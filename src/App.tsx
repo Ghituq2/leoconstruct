87473abcb2a8932ca46c2fb8e5822db9859f1999
unknown <Ghituq2>
2025-05-19 20:59:46 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
